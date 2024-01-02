7daf6119a22c88bb2f5e09e3a2bf328e7ba92b1b
xdbb1234 <zhanglin1232021@126.com>
2024-01-02 16:22:01 +0800

    first commit

---
+++ models/archs/DBPN.py
@@ -0,0 +1,211 @@
+import paddle
+import paddle.nn as nn
+import paddle.nn.functional as F
+
+class ConvBlock(nn.Layer):
+    def __init__(self, input_size, output_size, kernel_size=3, stride=1, padding=1, bias=True, activation='prelu', norm=None):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Conv2D(input_size, output_size, kernel_size, stride, padding)
+
+        self.norm = norm
+        if self.norm =='batch':
+            self.bn = nn.BatchNorm2D(output_size)
+        elif self.norm == 'instance':
+            self.bn = nn.InstanceNorm2D(output_size)
+
+        self.activation = activation
+        if self.activation == 'relu':
+            self.act = nn.ReLU(True)
+        elif self.activation == 'prelu':
+            self.act = nn.PReLU()
+        elif self.activation == 'lrelu':
+            self.act = nn.LeakyReLU(0.2, True)
+        elif self.activation == 'tanh':
+            self.act = nn.Tanh()
+        elif self.activation == 'sigmoid':
+            self.act = nn.Sigmoid()
+
+    def forward(self, x):
+        if self.norm is not None:
+            out = self.bn(self.conv(x))
+        else:
+            out = self.conv(x)
+
+        if self.activation is not None:
+            return self.act(out)
+        else:
+            return out
+
+class DeconvBlock(nn.Layer):
+    def __init__(self, input_size, output_size, kernel_size=4, stride=2, padding=1, bias=True, activation='prelu', norm=None):
+        super(DeconvBlock, self).__init__()
+        self.deconv = nn.Conv2DTranspose(input_size, output_size, kernel_size, stride, padding)
+
+        self.norm = norm
+        if self.norm == 'batch':
+            self.bn = nn.BatchNorm2D(output_size)
+        elif self.norm == 'instance':
+            self.bn = nn.InstanceNorm2D(output_size)
+
+        self.activation = activation
+        if self.activation == 'relu':
+            self.act = nn.ReLU(True)
+        elif self.activation == 'prelu':
+            self.act = nn.PReLU()
+        elif self.activation == 'lrelu':
+            self.act = nn.LeakyReLU(0.2, True)
+        elif self.activation == 'tanh':
+            self.act = nn.Tanh()
+        elif self.activation == 'sigmoid':
+            self.act = nn.Sigmoid()
+
+    def forward(self, x):
+        if self.norm is not None:
+            out = self.bn(self.deconv(x))
+        else:
+            out = self.deconv(x)
+
+        if self.activation is not None:
+            return self.act(out)
+        else:
+            return out
+
+class UpBlock(nn.Layer):
+    def __init__(self, num_filter, kernel_size=8, stride=4, padding=2, bias=True, activation='prelu', norm=None):
+        super(UpBlock, self).__init__()
+        self.up_conv1 = DeconvBlock(num_filter, num_filter, kernel_size, stride, padding, activation, norm=None)
+        self.up_conv2 = ConvBlock(num_filter, num_filter, kernel_size, stride, padding, activation, norm=None)
+        self.up_conv3 = DeconvBlock(num_filter, num_filter, kernel_size, stride, padding, activation, norm=None)        
+
+    def forward(self, x):
+    	h0 = self.up_conv1(x)
+    	l0 = self.up_conv2(h0)
+    	h1 = self.up_conv3(l0 - x)
+    	return h1 + h0
+
+class DownBlock(nn.Layer):
+    def __init__(self, num_filter, kernel_size=8, stride=4, padding=2, bias=True, activation='prelu', norm=None):
+        super(DownBlock, self).__init__()
+        self.down_conv1 = ConvBlock(num_filter, num_filter, kernel_size, stride, padding, activation, norm=None)
+        self.down_conv2 = DeconvBlock(num_filter, num_filter, kernel_size, stride, padding, activation, norm=None)
+        self.down_conv3 = ConvBlock(num_filter, num_filter, kernel_size, stride, padding, activation, norm=None)
+
+    def forward(self, x):   #x:Size([1, 64, 160, 160])
+    	l0 = self.down_conv1(x) #Size([1, 64, 40, 40])
+    	h0 = self.down_conv2(l0)    #Size([1, 64, 160, 160])
+    	l1 = self.down_conv3(h0 - x)
+    	return l1 + l0
+
+class D_DownBlock(nn.Layer):
+    def __init__(self, num_filter, kernel_size=8, stride=4, padding=2, num_stages=1, bias=True, activation='prelu', norm=None):
+        super(D_DownBlock, self).__init__()
+        self.conv = ConvBlock(num_filter*num_stages, num_filter, 1, 1, 0, activation, norm=None)
+        self.down_conv1 = ConvBlock(num_filter, num_filter, kernel_size, stride, padding, activation, norm=None)
+        self.down_conv2 = DeconvBlock(num_filter, num_filter, kernel_size, stride, padding, activation, norm=None)
+        self.down_conv3 = ConvBlock(num_filter, num_filter, kernel_size, stride, padding, activation, norm=None)
+
+    def forward(self, x):
+    	x = self.conv(x)
+    	l0 = self.down_conv1(x)
+    	h0 = self.down_conv2(l0)
+    	l1 = self.down_conv3(h0 - x)
+    	return l1 + l0
+class D_UpBlock(nn.Layer):
+    def __init__(self, num_filter, kernel_size=8, stride=4, padding=2, num_stages=1, bias=True, activation='prelu', norm=None):
+        super(D_UpBlock, self).__init__()
+        self.conv = ConvBlock(num_filter*num_stages, num_filter, 1, 1, 0, activation, norm=None)
+        self.up_conv1 = DeconvBlock(num_filter, num_filter, kernel_size, stride, padding, activation, norm=None)
+        self.up_conv2 = ConvBlock(num_filter, num_filter, kernel_size, stride, padding, activation, norm=None)
+        self.up_conv3 = DeconvBlock(num_filter, num_filter, kernel_size, stride, padding, activation, norm=None)
+
+    def forward(self, x):
+    	x = self.conv(x)
+    	h0 = self.up_conv1(x)
+    	l0 = self.up_conv2(h0)
+    	h1 = self.up_conv3(l0 - x)
+    	return h1 + h0
+
+class Net(nn.Layer):
+    def __init__(self, scale_factor):
+        super(Net, self).__init__()
+        num_channels=3
+        base_filter=64
+        feat=256
+        num_stages=7
+        if scale_factor == 2:
+            kernel = 6
+            stride = 2
+            padding = 2
+        elif scale_factor == 4:
+            kernel = 8
+            stride = 4
+            padding = 2
+        elif scale_factor == 8:
+            kernel = 12
+            stride = 8
+            padding = 2
+
+        # Initial Feature Extraction
+        self.feat0 = ConvBlock(num_channels, feat, 3, 1, 1, activation='prelu', norm=None)
+        self.feat1 = ConvBlock(feat, base_filter, 1, 1, 0, activation='prelu', norm=None)
+        # Back-projection stages
+        self.up1 = UpBlock(base_filter, kernel, stride, padding)
+        self.down1 = DownBlock(base_filter, kernel, stride, padding)
+        self.up2 = UpBlock(base_filter, kernel, stride, padding)
+        self.down2 = D_DownBlock(base_filter, kernel, stride, padding, 2)
+        self.up3 = D_UpBlock(base_filter, kernel, stride, padding, 2)
+        self.down3 = D_DownBlock(base_filter, kernel, stride, padding, 3)
+        self.up4 = D_UpBlock(base_filter, kernel, stride, padding, 3)
+        self.down4 = D_DownBlock(base_filter, kernel, stride, padding, 4)
+        self.up5 = D_UpBlock(base_filter, kernel, stride, padding, 4)
+        self.down5 = D_DownBlock(base_filter, kernel, stride, padding, 5)
+        self.up6 = D_UpBlock(base_filter, kernel, stride, padding, 5)
+        self.down6 = D_DownBlock(base_filter, kernel, stride, padding, 6)
+        self.up7 = D_UpBlock(base_filter, kernel, stride, padding, 6)
+        # Reconstruction
+        self.output_conv = ConvBlock(num_stages * base_filter, num_channels, 3, 1, 1, activation=None, norm=None)
+
+
+
+    def forward(self, x):
+        x = self.feat0(x)
+        x = self.feat1(x)
+
+        h1 = self.up1(x)
+        l1 = self.down1(h1)
+        h2 = self.up2(l1)
+
+        concat_h = paddle.concat((h2, h1), 1)
+        l = self.down2(concat_h)
+
+        concat_l = paddle.concat((l, l1), 1)
+        h = self.up3(concat_l)
+
+        concat_h = paddle.concat((h, concat_h), 1)
+        l = self.down3(concat_h)
+
+        concat_l = paddle.concat((l, concat_l), 1)
+        h = self.up4(concat_l)
+
+        concat_h = paddle.concat((h, concat_h), 1)
+        l = self.down4(concat_h)
+
+        concat_l = paddle.concat((l, concat_l), 1)
+        h = self.up5(concat_l)
+
+        concat_h = paddle.concat((h, concat_h), 1)
+        l = self.down5(concat_h)
+
+        concat_l = paddle.concat((l, concat_l), 1)
+        h = self.up6(concat_l)
+
+        concat_h = paddle.concat((h, concat_h), 1)
+        l = self.down6(concat_h)
+
+        concat_l = paddle.concat((l, concat_l), 1)
+        h = self.up7(concat_l)
+
+        concat_h = paddle.concat((h, concat_h), 1)
+        x = self.output_conv(concat_h)
+
+        return x
